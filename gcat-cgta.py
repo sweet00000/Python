9fcfb1207e4ea3308b461d8e8bdad45401100871
sweet00000 <88102370+sweet00000@users.noreply.github.com>
2021-07-28 11:51:36 -0400

    Update gcat-cgta.py

--- gcat-cgta.py
+++ gcat-cgta.py
@@ -1,86 +1,8 @@
 while True:
 
 
-	bases = input("Enter bases:") #String of Base Characters
+	bases = input("Enter bases:") #string of base characters
  
-	transtable = bases.maketrans("GCAT", "CGTA") #Defining each swap
+	transtable = bases.maketrans("GCAT", "CGTA") #defining each character swap
 
 	print(bases.translate(transtable))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	#new_bases=bases.replace("g", "c").replace("t", "a")
-
-
-	#print(new_bases)
-
-
-#new_bases=bases.replace("c", "g")
-#new_bases=bases.replace("a", "t")
-#new_bases=bases.replace("t", "a")
-
-#, ("c", "g"), ("a", "t"), ("t", "a")
-
-
-#if bases == ("A" or "a"):
-#	print("T" or "t")
-
-#elif bases == ("T" or "t"):
-#	print("A")
-
-#elif (bases == ("G" or "g")):
-#	print("C")
-
-#elif (bases == ("C" or "c")):
-#	print("G")
-
-
-
-
-# get the sequence from the user:
-
-	#dna_seq = input("Please enter your sequence here: ")
-
-# make a for loop to read the seq one nucleotide at a time and add each one in a new variable
-
-	#compliment = ""
-
-	#for n in dna_seq:
-
-	  #  if n == "A":
-	       # compliment = compliment + "T"
-	  #  elif n == "T":
-	        #compliment = compliment + "A"
-	  #  elif n == "G":
-	        #compliment = compliment + "C"
-	  #  elif n == "C":
-#	        compliment = compliment + "G"
-
-	#print(compliment)
