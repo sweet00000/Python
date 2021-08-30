1d6062564738a31a93dc3b60da35aed8f7a115d1
sweet00000 <88102370+sweet00000@users.noreply.github.com>
2021-08-30 11:37:22 -0400

    Update Reverse_Transcription.py

--- Reverse_Transcription.py
+++ Reverse_Transcription.py
@@ -1,7 +1,7 @@
-while True:
+while True:                                           #loop to infinitly ask prompt until program is exited
 
-	bases = input("Enter bases:") #string of base characters
+	bases = input("Enter bases:")                 #ask for and input with the prompt "Enter bases:", if a string of base characters is entered make that string the variable "bases"
  
-	transtable = bases.maketrans("GCAT", "CGTA") #defining each character swap
+	transtable = bases.maketrans("GCAT", "CGTA")  #make a translation table and apply it to the string entered above, have this be the variable "transtable"
 
-	print(bases.translate(transtable))
+	print(bases.translate(transtable))            #print the translated string
